efe70b48ccb2bb353883e1353cbfaa47b3ece814
Michel Chaghoury <michel.e.chaghoury@gmail.com>
2022-02-28 15:42:54 +0200

    Add Swagger Docs to API.
    Add Swashbuckle/Swagger with Auth Token Configuration for documentation and testing.

---
+++ RMSWebServices/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace RMSWebServices.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters is null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "Access Token",
+                required = false,
+                type = "string",
+                @default = "Bearer "
+            });
+        }
+    }
+}
